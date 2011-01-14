df0c9c6f0168a916416e9de1eaa3dea68d7a4c8f
macrule <macrule2001@yahoo.de>
2011-01-14 13:25:25 +0100

    Changed backend method getUPnPHost() to return empty string, if it can't find the correct one.

--- backend/jCinemaRPC/WDTV.pm
+++ backend/jCinemaRPC/WDTV.pm
@@ -5,12 +5,21 @@ use base qw(jCinemaRPC);
 
 sub getUPnPHost : Public {
 	my $port = `lsof -a -i4 -sTCP:LISTEN -c DMARender -F n`;
+	if (!defined $port) {
+		return "";
+	}
+	
 	$port =~ /n\*:(\d*)$/;
 	$port = $1;
 	
 	my $ip = `ipaddr show scope global`;
+	if (!defined $ip) {
+		return "";
+	}
+	
 	$ip =~ /inet ([^\/]*)\//;
 	$ip = $1;
+	
 	return "$ip:$port";
 }
 
