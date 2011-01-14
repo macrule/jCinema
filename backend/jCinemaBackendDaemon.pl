26812c927d35c553e773701c34b59f46f56ed8dc
macrule <macrule2001@yahoo.de>
2011-01-14 14:55:29 +0100

    Backend server didn't encode strings as utf-8

--- backend/jCinemaBackendDaemon.pl
+++ backend/jCinemaBackendDaemon.pl
@@ -10,6 +10,7 @@ use lib abs_path(dirname($0)).'/lib';
 
 use JSON::RPC::Server::Daemon;
 my $server = JSON::RPC::Server::Daemon->new(LocalPort => 8080, Reuse => 1);
+$server->json->utf8(0);
 $server->dispatch({
 	'/jCinemaRPC'		=> 'jCinemaRPC',
 	'/jCinemaRPC/WDTV'	=> 'jCinemaRPC::WDTV',
