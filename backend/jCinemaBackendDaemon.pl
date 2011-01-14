db24c0426541fb4bea38d4e40a3ba706a30eeafa
macrule <macrule2001@yahoo.de>
2011-01-14 11:43:01 +0100

    Changed backend daemon, so it can be run from any working directory

--- backend/jCinemaBackendDaemon.pl
+++ backend/jCinemaBackendDaemon.pl
@@ -1,6 +1,12 @@
 #!/usr/bin/perl -w
 
-use lib './lib';
+use strict;
+use warnings;
+
+use File::Basename;
+use Cwd 'abs_path';
+use lib abs_path(dirname($0));
+use lib abs_path(dirname($0)).'/lib';
 
 use JSON::RPC::Server::Daemon;
 my $server = JSON::RPC::Server::Daemon->new(LocalPort => 8080);
