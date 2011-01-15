e3b2d01c01f555d9aa5c006053b56f40c9b14228
macrule <macrule2001@yahoo.de>
2011-01-16 00:51:43 +0100

    Added key mappings for all WDTV keys to the Desktop platform key mappings, so they can be simulated.

--- webapp/jCinema/platforms/Desktop/KeyHandlerImpl.js
+++ webapp/jCinema/platforms/Desktop/KeyHandlerImpl.js
@@ -17,6 +17,11 @@
 jCinema.platform.Desktop.KeyHandlerImpl = function () {
 	
 	var mapKeyEventTojCinemaEvent = function (keyEvent) {
+		// do not map key presses with Ctrl or Cmd (Apple) key pressed
+		if (keyEvent.ctrlKey || keyEvent.metaKey) {
+			return null;
+		}
+		
 		if (keyEvent.keyCode == 38) return jCinema.IKeyHandler.KeyEvent.Up;
 		if (keyEvent.keyCode == 40) return jCinema.IKeyHandler.KeyEvent.Down;
 		if (keyEvent.keyCode == 37) return jCinema.IKeyHandler.KeyEvent.Left;
@@ -29,23 +34,27 @@ jCinema.platform.Desktop.KeyHandlerImpl = function () {
 		// Space bar is Play/Pause button
 		if (keyEvent.keyCode == 32) return jCinema.IKeyHandler.KeyEvent.PlayPause;
 		
+		if (keyEvent.keyCode == 'O'.charCodeAt(0)) return jCinema.IKeyHandler.KeyEvent.Option;
 		
-		// TODO:
+		if (keyEvent.keyCode == 'P'.charCodeAt(0)) return jCinema.IKeyHandler.KeyEvent.Power;
+		if (keyEvent.keyCode == 'H'.charCodeAt(0)) return jCinema.IKeyHandler.KeyEvent.Home;
 		
-		//if (keyEvent.keyCode == 18) return jCinema.IKeyHandler.KeyEvent.Option;
+		if (keyEvent.keyCode == 'T'.charCodeAt(0)) return jCinema.IKeyHandler.KeyEvent.Stop;
+		if (keyEvent.keyCode == 'F'.charCodeAt(0)) return jCinema.IKeyHandler.KeyEvent.FastForward;
+		if (keyEvent.keyCode == 'R'.charCodeAt(0)) return jCinema.IKeyHandler.KeyEvent.Reverse;
 		
-		//if (keyEvent.originalEvent.keyIdentifier == 'U+F00F') return jCinema.IKeyHandler.KeyEvent.Power;
-		//if (keyEvent.keyCode == 36) return jCinema.IKeyHandler.KeyEvent.Home;
+		if (keyEvent.keyCode == 'B'.charCodeAt(0)) return jCinema.IKeyHandler.KeyEvent.Previous;
+		if (keyEvent.keyCode == 'N'.charCodeAt(0)) return jCinema.IKeyHandler.KeyEvent.Next;
 		
-		//if (keyEvent.originalEvent.keyIdentifier == 'U+1000081') return jCinema.IKeyHandler.KeyEvent.Stop;
-		//if (keyEvent.originalEvent.keyIdentifier == 'U+F05A') return jCinema.IKeyHandler.KeyEvent.FastForward;
-		//if (keyEvent.originalEvent.keyIdentifier == 'U+F059') return jCinema.IKeyHandler.KeyEvent.Reverse;
+		if (keyEvent.keyCode == 'E'.charCodeAt(0)) return jCinema.IKeyHandler.KeyEvent.Eject;
+		if (keyEvent.keyCode == 'S'.charCodeAt(0)) return jCinema.IKeyHandler.KeyEvent.Search;
 		
-		//if (keyEvent.originalEvent.keyIdentifier == 'U+1000082') return jCinema.IKeyHandler.KeyEvent.Previous;
-		//if (keyEvent.originalEvent.keyIdentifier == 'U+1000083') return jCinema.IKeyHandler.KeyEvent.Next;
+		// Home key also works
+		if (keyEvent.keyCode == 36) return jCinema.IKeyHandler.KeyEvent.Home;
 		
-		//if (keyEvent.originalEvent.keyIdentifier == 'U+F057') return jCinema.IKeyHandler.KeyEvent.Eject;
-		//if (keyEvent.originalEvent.keyIdentifier == 'U+1000094') return jCinema.IKeyHandler.KeyEvent.Search;
+		// Page Down/Up works as Prev/Next
+		if (keyEvent.keyCode == 33) return jCinema.IKeyHandler.KeyEvent.Previous;
+		if (keyEvent.keyCode == 34) return jCinema.IKeyHandler.KeyEvent.Next;
 		
 		return null;
 	};
