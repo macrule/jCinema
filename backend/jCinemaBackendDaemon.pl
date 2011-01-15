84b8394f7bf7d85c43d68c5dab705f8563627501
macrule <macrule2001@yahoo.de>
2011-01-15 02:23:39 +0100

    Made backend daemon more easily debuggable/testable by adding "use diagnostics" and loading dependent modules early.

--- backend/jCinemaBackendDaemon.pl
+++ backend/jCinemaBackendDaemon.pl
@@ -2,11 +2,16 @@
 
 use strict;
 use warnings;
+use diagnostics;
 
 use File::Basename;
 use Cwd 'abs_path';
 use lib abs_path(dirname($0));
 use lib abs_path(dirname($0)).'/lib';
+
+# "use" these, to find compilation errors at once
+use jCinemaRPC;
+use jCinemaRPC::WDTV;
 
 use JSON::RPC::Server::Daemon;
 my $server = JSON::RPC::Server::Daemon->new(LocalPort => 8080, Reuse => 1);
