7f4bf778491d952fb5e56742cbf85256f02e1d47
macrule <macrule2001@yahoo.de>
2011-01-17 23:10:34 +0100

    Moved defaults from config.js to config.defaults.js, and added comment about use of multiple patterns.

--- webapp/config.js
+++ webapp/config.js
@@ -5,8 +5,4 @@ $.extend(jCinema.options, {
 	Platform:		'Desktop',
 	//BackEndHost:	'127.0.0.1:8080',
 	//MediaSearchPath: '/tmp/media/usb',
-	FolderImagePathPattern:     '{path}/{name}.jpg;{path}/folder.jpg',
-	ThumbnailImagePathPattern:  '{dir}/_MovieSheets/{name}/thumb.jpg;{dir}/{name}.jpg;{dir}/folder.jpg',
-	MovieSheetImagePathPattern: '{dir}/_MovieSheets/{name}/sheet.jpg;{dir}/{name}{suffix}_sheet.jpg;{dir}/wd_tv.jpg',
-	PatternSeparator: 	    ';',
 });
--- webapp/jCinema/config.defaults.js
+++ webapp/jCinema/config.defaults.js
@@ -14,7 +14,11 @@ $.extend(jCinema.options, {
 	//  - {dir}    full path to parent directory of folder or movie
 	//  - {name}   file name without suffix
 	//  - {suffix} file suffix including the dot
-	FolderImagePathPattern:     '{path}/folder.jpg',
-	ThumbnailImagePathPattern:  '{dir}/_MovieSheets/{name}/thumb.jpg',
-	MovieSheetImagePathPattern: '{dir}/_MovieSheets/{name}/sheet.jpg',
+	// you can specify multiple patterns, separated by
+	// jCinema.options.PatternSeparator (default is semicolon).
+	// the first one that matches will be used.
+	FolderImagePathPattern:     '{path}/{name}.jpg;{path}/folder.jpg',
+	ThumbnailImagePathPattern:  '{dir}/_MovieSheets/{name}/thumb.jpg;{dir}/{name}.jpg;{dir}/folder.jpg',
+	MovieSheetImagePathPattern: '{dir}/_MovieSheets/{name}/sheet.jpg;{dir}/{name}{suffix}_sheet.jpg;{dir}/wd_tv.jpg',
+	PatternSeparator:           ';',
 });
